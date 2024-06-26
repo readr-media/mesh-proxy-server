92042a472d1adec8c6092e70f76ad1e795451316
Dylan Liu <gaespa007648@gmail.com>
2024-06-26 11:06:03 +0800

    feat: verify token by firebase_admin

--- src/auth.py
+++ src/auth.py
@@ -5,12 +5,9 @@ class _Authentication():
     def __init__(self):
         self.firebase_app = None
     def verifyTokenByFirebaseAdmin(self):
-        firebase_project_id = os.environ.get('FIREBASE_PROJECT_ID', '')
         if self.firebase_app==None:
             print("initialize app by firebase admin")
-            self.firebase_app = firebase_admin.initialize_app({
-                "projectId": firebase_project_id
-            })
+            self.firebase_app = firebase_admin.initialize_app()
 
 ### singleton design patter, you should import Authentication object to use
 Authentication = _Authentication()
