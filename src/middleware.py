83d1c13d98dc03bec9d560e60d5c0d3d9d73748c
Dylan Liu <gaespa007648@gmail.com>
2024-07-19 12:37:12 +0800

    feat: add exception to middleware

--- src/middleware.py
+++ src/middleware.py
@@ -18,41 +18,46 @@ def middleware_story_acl(request: Request):
     jwt_token = request.headers.get("Authorization", None)
     if jwt_token==None:
         return acl_header, error_msg
-    jwt_token = json.loads(jwt_token)
     
-    ### check the content in the jwt_token
-    result = verify_jwt_token(jwt_token)
-    if result==False:
-        error_msg = "Invalid jwt token"
-        return acl_header, error_msg
-    
-    ### unwrap transactions from jwt_token['claim'] and wrap them into acl_header
-    txs = jwt_token.get('claim', {}).get('txs', []) # txs = transacions
-    unlock_all_txs, unlock_media_txs, unlock_single_txs = [], [], []
-    # categorize all the transactions
-    for tx in txs:
-        policy_type = tx.get('policy',{}).get('type', '')
-        unlockSingle = tx.get('policy', {}).get('unlockSingle', False)
-        if policy_type == 'unlock_all_publishers':
-            unlock_all_txs.append(tx)
-        if policy_type == 'unlock_one_publisher':
-            if unlockSingle==False:
-                unlock_media_txs.append(tx)
-            else:
-                unlock_single_txs.append(tx)
-    # wrap acl header
-    if len(unlock_all_txs)>0:
-        acl_header = {
-            "x-access-token-scope": "mesh:member-stories:all"
-        }
-    else:
-        mediaArr = [tx.get('policy', {}).get('publisher', {}).get('id') for tx in unlock_media_txs]
-        mediaArr_str = ','.join(mediaArr)
-        storyArr = [tx.get('unlockStory',{}).get('id') for tx in unlock_single_txs]
-        storyArr_str = ','.join(storyArr)
-        acl_header = {
-            "x-access-token-scope": "mesh:member-stories:media",
-            "x-access-token-media": mediaArr_str,
-            "x-access-token-story": storyArr_str,
-        }
+    try:
+        ### if jwt_token is invalid, this line will cause exception
+        jwt_token = json.loads(jwt_token)
+        ### check the content in the jwt_token
+        result = verify_jwt_token(jwt_token)
+        if result==False:
+            error_msg = "Invalid jwt token"
+            return acl_header, error_msg
+        
+        ### unwrap transactions from jwt_token['claim'] and wrap them into acl_header
+        txs = jwt_token.get('claim', {}).get('txs', []) # txs = transacions
+        unlock_all_txs, unlock_media_txs, unlock_single_txs = [], [], []
+        # categorize all the transactions
+        for tx in txs:
+            policy_type = tx.get('policy',{}).get('type', '')
+            unlockSingle = tx.get('policy', {}).get('unlockSingle', False)
+            if policy_type == 'unlock_all_publishers':
+                unlock_all_txs.append(tx)
+            if policy_type == 'unlock_one_publisher':
+                if unlockSingle==False:
+                    unlock_media_txs.append(tx)
+                else:
+                    unlock_single_txs.append(tx)
+        # wrap acl header
+        if len(unlock_all_txs)>0:
+            acl_header = {
+                "x-access-token-scope": "mesh:member-stories:all"
+            }
+        else:
+            mediaArr = [tx.get('policy', {}).get('publisher', {}).get('id') for tx in unlock_media_txs]
+            mediaArr_str = ','.join(mediaArr)
+            storyArr = [tx.get('unlockStory',{}).get('id') for tx in unlock_single_txs]
+            storyArr_str = ','.join(storyArr)
+            acl_header = {
+                "x-access-token-scope": "mesh:member-stories:media",
+                "x-access-token-media": mediaArr_str,
+                "x-access-token-story": storyArr_str,
+            }
+    except Exception as e:
+        print("middleware_story_acl error: ", e)
+        return None, None
     return acl_header, None
