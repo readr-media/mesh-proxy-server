7008b37d52f490b8e763ee81d2c5a3f126075bc5
Dylan Liu <gaespa007648@gmail.com>
2024-11-27 11:33:23 +0800

    refactor: notify content, move into pubsub

--- src/notify.py
+++ src/notify.py
@@ -23,67 +23,6 @@ query Members($where: MemberWhereInput!){
 }
 '''
 
-gql_notify_member = '''
-query Member{{
-  member(where: {{id: {ID} }}){{
-    id
-    customId
-    name
-    avatar
-  }}
-}}
-'''
-
-gql_notify_story = '''
-query Story{{
-  story(where: {{id: {ID} }}){{
-    id
-    title
-    url
-    source{{
-      id
-      customId
-      title
-    }}
-    commentCount(where: {{is_active: {{equals: true}} }})
-  }}
-}}
-'''
-
-gql_notify_comment = '''
-query Comment{{
-  comment(where: {{id: {ID} }}){{
-    id
-    content
-  }}
-}}
-'''
-
-gql_notify_collection = '''
-query Collection{{
-  collection(where: {{id: {ID} }}){{
-    id
-    title
-  }}
-}}
-'''
-
-def get_objective_content(gql_client, objective, targetId):
-    content = None
-    if objective=="story":
-        gql_string = gql_notify_story.format(ID=targetId)
-        data = gql_client.execute(gql(gql_string))
-        content = data['story']
-    if objective=="comment":
-        gql_string = gql_notify_comment.format(ID=targetId)
-        data = gql_client.execute(gql(gql_string))
-        content = data['comment']        
-    if objective=="collection":
-        gql_string = gql_notify_collection.format(ID=targetId)
-        data = gql_client.execute(gql(gql_string))
-        content = data['collection'] 
-    return content
-
 def get_notifies(db, memberId: str, index: int=0, take: int=10):
     MESH_GQL_ENDPOINT = os.environ['MESH_GQL_ENDPOINT']
     col_notify = db.notifications
@@ -174,7 +113,9 @@ def get_notifies(db, memberId: str, index: int=0, take: int=10):
                 "notifiers": notifiers,
                 "ts": notify["ts"]
             }
-            content = get_objective_content(gql_client, objective, targetId) # get content information about the objective targetId
+            
+            # content is optional field, which is used as appendix
+            content = notify.get('content', None)
             if content:
                 full_notify['content'] = content
             
